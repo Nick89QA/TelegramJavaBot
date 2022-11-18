2105ab7efdb3682f1bee5d156b4d0c90e8ddc58f
Nickolay Petrych <npetrych@alfabank.ru>
2022-11-18 13:04:23 +0500

    add classes and methods

--- dispatcher/src/main/java/runner/controller/TelegramBot.java
+++ dispatcher/src/main/java/runner/controller/TelegramBot.java
@@ -1,14 +1,20 @@
 package runner.controller;
 
+import lombok.extern.log4j.Log4j;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
 
 
 @Component //spring создаст bin
+@Log4j //аннотация которая иницилизирует loger в классе
 public class TelegramBot extends TelegramLongPollingBot {
 
     /**
@@ -18,7 +24,16 @@ public class TelegramBot extends TelegramLongPollingBot {
     private String botName;
     @Value("${bot.token}")
     private String botToken;
-    private static final Logger log = Logger.getLogger(TelegramBot.class);
+    private UpdateController updateController;
+
+    public TelegramBot(UpdateController updateController){
+        this.updateController = updateController;
+    }
+
+    @PostConstruct
+    public void init(){
+        updateController.registerBot(this);
+    }
 
 
     @Override
@@ -35,5 +50,24 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         var originalMessage = update.getMessage();
         log.debug(originalMessage.getText());
+
+        /**
+         * метод который отправляет в ответ сообщение
+         */
+        var response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Привет Лошадка как дела");
+        sendAnswerMessage(response);
     }
+
+  public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try{
+                execute(message);
+            }catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
+  }
+
 }
